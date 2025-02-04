1a7a7ab7b52f60434bba4358aae247ca7756ecfe
Vishesh Srivastava <vishesh.srivastava@coditas.com>
2025-02-04 14:25:02 +0530

    LinkedList methods

---
+++ Understanding Data Structures/src/LinkedList.java
@@ -0,0 +1,71 @@
+public class LinkedList
+{
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node
+    {
+        int value;
+        Node next;
+
+        Node (int value)
+        {
+            this.value=value;
+        }
+    }
+    public LinkedList (int value)
+    {
+        Node newNode=new Node(value);
+        head = newNode;
+        tail=newNode;
+        length=1;
+    }
+//Method to print the linked list
+    public void printList()
+    {
+        Node temp=head;
+        while(temp != null)
+        {
+            System.out.print(temp.value+"-->");
+            temp=temp.next;
+        }
+    }
+//Method to append the linkedlist
+    public void append (int value)
+    {
+        Node newNode=new Node(value);
+        if(length==0)
+        {
+            head = newNode;
+            tail=newNode;
+        }
+        else
+        {
+            tail.next=newNode;
+            tail=newNode;
+        }
+        length++;
+    }
+//Method to remove the last node of the linked list
+    public Node removeLast()
+    {
+        if(length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null)
+        {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length --;
+        if(length == 0)
+        {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+}
---
+++ Understanding Data Structures/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+
+        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.append(4);
+//        myLinkedList.append(5);
+       myLinkedList.printList();
+       System.out.println();
+       myLinkedList.removeLast();
+       myLinkedList.printList();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 4
+            Tail: 4
+            Length: 1
+
+            Linked List:
+            4
+
+        */
+
+    }
+
+}
